974bdead16af4e489dfe3227b3944f6e31653f07
yonatan alebachew <yonatanalebachew7@gmail.com>
2024-04-14 08:21:43 -0900

    username edited

--- poem/lib/widgets/username.dart
+++ poem/lib/widgets/username.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:poem/screens/mixin/validator.dart';
+import '../widgets/mixin/validator.dart';
 
 class Username extends StatefulWidget {
   const Username({super.key});
