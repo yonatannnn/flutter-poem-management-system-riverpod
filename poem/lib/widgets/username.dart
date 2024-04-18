3232cda3ea052e93139dcfef917aa39277f3f526
Aderajew Yeshiwendm <adeyeshi294@gmail.com>
2024-04-18 13:34:34 +0300

    username in a separete file

---
+++ poem/lib/widgets/username.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Username extends StatefulWidget {
+  const Username({super.key});
+  @override
+  State<StatefulWidget> createState() {
+    return _usernameState();
+  }
+}
+
+class _usernameState extends State<Username> {
+  final TextEditingController usernameController = TextEditingController();
+  String? usernameError;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+        padding: const EdgeInsets.only(right: 10),
+        child: TextFormField(
+          controller: usernameController,
+          decoration: InputDecoration(
+            prefixIcon: const Icon(
+              Icons.person,
+              color: Colors.blue,
+            ),
+            hintText: 'your username ...',
+            hintStyle: TextStyle(color: Colors.grey.withOpacity(0.5)),
+            errorText: usernameError,
+            border: const OutlineInputBorder(),
+          ),
+          style: const TextStyle(
+            color: Colors.black87,
+          ),
+          onChanged: (value) {
+            setState(() {});
+          },
+        ));
+  }
+}
