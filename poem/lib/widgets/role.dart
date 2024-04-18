3e94b13c68116d4dd47935f2762eeffc5472dda8
Aderajew Yeshiwendm <adeyeshi294@gmail.com>
2024-04-18 13:36:03 +0300

    role.dart

---
+++ poem/lib/widgets/role.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+class Role extends StatefulWidget {
+  final void Function(String?) onChanged;
+  const Role({super.key, required this.onChanged});
+
+  @override
+  State<Role> createState() => _RoleState();
+}
+
+class _RoleState extends State<Role> {
+  final TextEditingController roleController = TextEditingController();
+  String? roleError;
+  String? role;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.only(right: 10),
+      child: TextFormField(
+        controller: roleController,
+        keyboardType: TextInputType.emailAddress,
+        decoration: InputDecoration(
+          hintText: 'poet, or enthusiast',
+          hintStyle: TextStyle(color: Colors.grey.withOpacity(0.5)),
+          errorText: roleError,
+          border: const OutlineInputBorder(),
+        ),
+        style: const TextStyle(
+          color: Colors.black87,
+        ),
+        onChanged: (value) {
+          setState(() {});
+          widget.onChanged(value);
+        },
+      ),
+    );
+  }
+}
