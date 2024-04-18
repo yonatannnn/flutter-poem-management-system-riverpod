861744e056ade39c64d78a03742691727adc30cb
Aderajew Yeshiwendm <adeyeshi294@gmail.com>
2024-04-18 13:51:11 +0300

    admin main page startup

---
+++ poem/lib/screens/main_page.dart
@@ -0,0 +1,111 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+import 'package:poem/screens/welcome_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Poem Management System',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+          appBarTheme: AppBarTheme(
+              centerTitle: true,
+              shadowColor: Colors.black,
+              toolbarHeight: 70,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20),
+              )),
+          primarySwatch: Colors.yellow,
+          useMaterial3: true,
+          fontFamily: 'Georgia',
+          scaffoldBackgroundColor: Colors.grey[200]),
+      home: const MyHomePage(),
+      routes: {
+        '/welcome': (context) => const Welcome(),
+      },
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  File? _image;
+  File? pickedImage;
+  String email = 'adeyeshi@gmail.com';
+  late final Function(int) onDelete;
+  late final Function(int) onFavorite;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        leading: Builder(builder: (BuildContext context) {
+          return IconButton(
+            onPressed: () {
+              Scaffold.of(context).openDrawer();
+            },
+            icon: const Icon(Icons.menu),
+          );
+        }),
+        title: TextField(
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.grey[300],
+            hintText: 'Search by title...',
+            prefixIcon: const Icon(Icons.search),
+            border: OutlineInputBorder(
+              // Border
+              borderRadius: BorderRadius.circular(10.0),
+              borderSide: BorderSide.none,
+            ),
+            contentPadding:
+                const EdgeInsets.symmetric(vertical: 12.0, horizontal: 16.0),
+          ),
+        ),
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.push(context,
+                    MaterialPageRoute(builder: (context) => const Welcome()));
+              },
+              icon: const Icon(Icons.logout))
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        tooltip: 'Add a poem',
+        backgroundColor: Colors.blueGrey,
+        onPressed: () {},
+        child: const Icon(
+          Icons.add,
+          color: Colors.white,
+          size: 40,
+        ),
+      ),
+      bottomNavigationBar: buildBottomNavigationBar(context),
+    );
+  }
+
+  BottomNavigationBar buildBottomNavigationBar(BuildContext context) {
+    return BottomNavigationBar(
+      items: const <BottomNavigationBarItem>[
+        BottomNavigationBarItem(icon: Icon(Icons.menu_book), label: 'Poems'),
+        BottomNavigationBarItem(
+            icon: Icon(
+              Icons.favorite,
+              color: Colors.red,
+            ),
+            label: 'My Favorite')
+      ],
+    );
+  }
+}
